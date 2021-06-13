0fe996f5c6b2d687c3027327456eedf42f5b3fa9
PranavBhatia <bhatiapranav1996@gmail.com>
2021-06-13 10:19:30 -0400

    Using marketData from service

--- src/app/crypto-prices/crypto-prices.component.ts
+++ src/app/crypto-prices/crypto-prices.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CryptoPricesService } from './crypto-prices.service';
 
 @Component({
   selector: 'app-crypto-prices',
   templateUrl: './crypto-prices.component.html',
-  styleUrls: ['./crypto-prices.component.css']
+  styleUrls: ['./crypto-prices.component.css'],
 })
 export class CryptoPricesComponent implements OnInit {
+  marketData: {
+    s: string;
+    n: string;
+    p: number;
+    mc: number;
+    pc: number;
+  }[] = [];
 
-  constructor() { }
+  constructor(private cryptoPricesService: CryptoPricesService) {}
 
   ngOnInit(): void {
+    this.marketData = this.cryptoPricesService.marketData;
   }
-
 }

a730f0143819391be36bd0b8d1802e81ef944b0c
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-09-19 21:49:25 -0400

    Login form complete

--- src/app/auth/login/login.module.ts
+++ src/app/auth/login/login.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
-import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
+    FormsModule,
     CommonModule,
-    LoginRoutingModule
-  ]
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    LoginRoutingModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}

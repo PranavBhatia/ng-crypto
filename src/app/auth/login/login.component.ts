630c1a19503e499b41c615c5fc7bb6460cbdcf05
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-12-01 22:15:44 -0500

    Get

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,16 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {}
 
   onLogin(loginForm: NgForm) {
     console.log(loginForm.value);
+    this.httpClient
+      .get('https://angular-crypto-test-default-rtdb.firebaseio.com/users.json')
+      .subscribe((users) => {
+        console.log(users);
+      });
   }
 }

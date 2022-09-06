caed5c7c29223d73f0cdca2d20664a31566c5cdb
Pranav Bhatia <bhatiapranav1996@gmail.com>
2022-09-05 21:30:43 -0400

    Login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -8,23 +10,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
   ngOnInit(): void {}
 
   onLogin(loginForm: NgForm) {
     console.log(loginForm.value);
     this.httpClient
-      .get(
-        'https://angular-crypto-test-default-rtdb.firebaseio.com/users.json',
-        {
-          params: new HttpParams()
-            .set('orderBy', '"email"')
-            .set('equalTo', `"${loginForm.value.email}"`),
-        }
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseApiKey}`,
+        {...loginForm.value, returnSecureToken: true}
       )
       .subscribe((users) => {
-        console.log(users);
+        this.router.navigate(['/']);
+        alert('Login successful!');
       });
   }
 }

9d21e74b2c654bf8ee5ed5522bd912b581240060
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2023-02-13 20:55:31 -0500

    Update packages

--- src/app/shared/firebase/firebase.module.ts
+++ src/app/shared/firebase/firebase.module.ts
@@ -9,29 +9,22 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     {
-      scopes: [
-        'public_profile',
-        'email',
-        'user_likes',
-        'user_friends'
-      ],
+      scopes: ['public_profile', 'email', 'user_likes', 'user_friends'],
       customParameters: {
-        'auth_type': 'reauthenticate'
+        auth_type: 'reauthenticate',
       },
-      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     },
     firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
     {
       requireDisplayName: false,
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
     },
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
   ],
   tosUrl: '<your-tos-link>',
   privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
-  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
 };
 
 @NgModule({
@@ -41,10 +34,6 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
     AngularFireModule.initializeApp(environment.firebase),
     FirebaseUIModule.forRoot(firebaseUiAuthConfig),
   ],
-  exports: [
-    FirebaseUIModule,
-    AngularFireModule,
-    AngularFireAuthModule,
-  ]
+  exports: [FirebaseUIModule, AngularFireModule, AngularFireAuthModule],
 })
-export class FirebaseModule { }
+export class FirebaseModule {}
